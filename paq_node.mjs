91160fac3baca7d813a09d035f29faed027821c7
Iskander <70473827+Iskander0@users.noreply.github.com>
2023-07-29 14:43:51 +0300

    add nodejs lib

---
+++ paq_node.mjs
@@ -0,0 +1,36 @@
+import { promises as fs } from 'fs';
+
+const init = async (name) => {
+  const buffer = await fs.readFile('./paq.wasm');
+  const mod = await WebAssembly.compile(buffer);
+  const wasm = (await WebAssembly.instantiate(mod)).exports;
+
+  return it => {
+    const n1 = it.length;
+    const p1 = wasm.__wbindgen_malloc(n1, 1);
+    const p = wasm.__wbindgen_add_to_stack_pointer(-16);
+    try {
+      new Uint8Array(wasm.memory.buffer).set(it, p1);
+      wasm[name](p, p1, n1);
+      const arr = new Int32Array(wasm.memory.buffer);
+      const p2 = arr[p/4]; const n2 = arr[p/4 + 1];
+      const res = new Uint8Array(wasm.memory.buffer).subarray(p2, p2 + n2).slice();
+      wasm.__wbindgen_free(p2, n2);
+      return res;
+    } finally {
+      wasm.__wbindgen_add_to_stack_pointer(16);
+    }
+  };
+};
+
+const paq = async (data) => {
+  const fn = await init('paq');
+  return fn(data);
+};
+
+const unpaq = async (data) => {
+  const fn = await init('unpaq');
+  return fn(data);
+};
+
+export { paq, unpaq };
